41d53f59e14cb1c6fb2ffd63ecc5b4c486101aa6
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-10-18 22:38:39 +0530

    gettingf used to doubly linked list structure

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,5 +1,31 @@
 public class DoublyLinkedList {
+
+    
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        /*
+         * 1. It is called two way linkedlist
+         * 2. Given a node, we can navigate list in both forward and backward direction, which is
+         *    not possible in singly linked list.
+         * 3. A node in singly linked list can only be deleted if we have a pointer to its previous
+         *    node. But in doubly linkedlist we can delete the node even if we don't have pointer to its
+         *    previous node.
+         * 4. ListNode in Doubly Linked List (link to previous node) <---- data ----> (link to next node)
+         */
+        DoublyLinkedList dll = new DoublyLinkedList();
+        ListNode headNode = new ListNode(1);
+        headNode.previous = null;
+        headNode.next = new ListNode(10);
     }
 }
