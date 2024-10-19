ee1cdbab87457c622db6d1bf79467b70b82866ff
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-10-19 19:32:20 +0530

    printElements, insertElementAtFirst, insertElementAtEnd

--- CircularSinglyLinkedList.java
+++ CircularSinglyLinkedList.java
@@ -25,5 +25,59 @@ public class CircularSinglyLinkedList {
         } else {
             System.out.println("Both are different though both have same data i.e. we are comparing their memeory address");
         }
+
+
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+        ListNode tail = fourth;
+
+        csll.printElements(tail);
+
+        // insert element at beginning
+        csll.insertElementAtFirst(tail, 0);
+        csll.printElements(tail);
+
+
+        // insert element at last
+        tail = csll.insertElementAtEnd(tail, 5);
+        csll.printElements(tail);
+
+    }
+
+
+    // traverse and print all elements from a given linked list
+    public void printElements(ListNode tail){
+        ListNode currentNode = tail.next;
+        while (currentNode != tail) {
+            System.out.print(currentNode.data + " ---> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println(currentNode.data);
+    }
+
+
+    // insert node at the beginning of circular linked list
+    public void insertElementAtFirst(ListNode tail, int data){
+        ListNode firstNode = tail.next;
+        ListNode newNode = new ListNode(data);
+        newNode.next = firstNode;
+        tail.next = newNode;
+    }
+
+    // insert node at the beginning of circular linked list
+    public ListNode insertElementAtEnd(ListNode tail, int data){
+        ListNode newNode = new ListNode(data);
+        ListNode firstNode = tail.next;
+        newNode.next = firstNode;
+        tail.next = newNode;
+        tail = newNode;
+        return tail;
     }
 }
