7f47b2350bc6ee4ad51b5b7dcd109fe4c3db299e
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-10-26 14:12:22 +0530

    stack operations

--- Stack.java
+++ Stack.java
@@ -1,4 +1,18 @@
+import java.util.EmptyStackException;
+
 public class Stack{
+    private ListNode top;
+    private int length;
+    
+    public static class ListNode {
+        private int data;
+        private ListNode next;
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     public static void main(String[] args) {
         /**
          * What is Stack?
@@ -9,5 +23,55 @@ public class Stack{
          * 
          * top --> 10 --> 8 --> 1 --> 11 --> 15 --> null
          */
+        
+        
+        
+        
+    }
+
+
+    public Stack(){
+        top.next = null;
+        length = 0;
+    }
+
+
+    public void push(int data){
+        ListNode temp = new ListNode(data);
+        temp.next = top;
+        top = temp;
+        length++;
+    }
+
+
+    public void pop(){
+        if (isEmpty()) {
+            throw new EmptyStackException();    
+        }
+
+        top = top.next;
+        length--;
+    }
+
+
+    public int peek(){
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        
+        return top.next.data;
+    }
+
+
+    public int length(){
+        return length;
+    }
+
+
+    public boolean isEmpty(){
+        if (length == 0) {
+            return true;
+        }
+        return false;
     }
 }
