e5e69e9b726f41b1509c5b439d42e4962c394604
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-10-26 12:17:19 +0530

    starting stack

---
+++ Stack.java
@@ -0,0 +1,13 @@
+public class Stack{
+    public static void main(String[] args) {
+        /**
+         * What is Stack?
+         * 
+         * 1. It is a linear data structure used for storing the data.
+         * 2. Its an ordered list in which insertion and deletion are done at one end, called as top
+         * 3. The last element inserted is the first one to be deleted. Hence, it is called as Last In First Out (LIFO) list.
+         * 
+         * top --> 10 --> 8 --> 1 --> 11 --> 15 --> null
+         */
+    }
+}
