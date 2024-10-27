72763a229685d4d896b557b2e14836baf72a73ae
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-10-27 20:57:51 +0530

    starting with queue

---
+++ Queue.java
@@ -0,0 +1,14 @@
+public class Queue {
+    public static void main(String[] args) {
+        /*
+         * 1. It is a linear data structure used for storing the data.
+         * 2. Its an ordered list in which insertion are done at one end,
+         *    called as rear and deletion are done at other end called as
+         *    front.
+         * 3. The first element inserted is the first one to be deleted.
+         *    Hence, its called as First In First Out (FIFO) list.
+         * 
+         *              front --> 10 --> 8 --> 1 --> 11 --> 15 --> rear
+         */
+    }   
+}
