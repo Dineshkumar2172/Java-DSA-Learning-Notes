764472ea2a0195004a379e8b51529e07ca1782d1
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-11-01 18:50:12 +0530

    binary trees

---
+++ BinaryTree.java
@@ -0,0 +1,12 @@
+public class BinaryTree {
+    public static void main(String[] args) {
+        /*
+         * What is a Tree?
+         *      1. Its a non linear data structure used for storing data.
+         *      2. It is made up of nodes and edges without having any cycle.
+         *      3. Each node in a tree can point to n number of nodes in a tree.
+         *  `   4. It is a way of representing hierarchical structure with a parent 
+         *         node called as root and many levels of additional nodes. 
+         */
+    }
+}
