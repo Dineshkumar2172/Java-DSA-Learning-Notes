7f067df5847f85396cabdcef0ff27e6d020526f2
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-11-01 19:15:50 +0530

    tree node implementation

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,4 +1,14 @@
 public class BinaryTree {
+
+    public class TreeNode {
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+        public TreeNode(int data){
+            this.data = data;
+        }   
+    }
+
     public static void main(String[] args) {
         /*
          * What is a Tree?
@@ -6,7 +16,13 @@ public class BinaryTree {
          *      2. It is made up of nodes and edges without having any cycle.
          *      3. Each node in a tree can point to n number of nodes in a tree.
          *  `   4. It is a way of representing hierarchical structure with a parent 
-         *         node called as root and many levels of additional nodes. 
+         *         node called as root and many levels of additional nodes.
+         * 
+         * 
+         * What is a Binary Tree?
+         *      1. A tree is called a Binary Tree, if each node has zero, one or two children.
+         * 
          */
+
     }
 }
