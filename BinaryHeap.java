b785f1383bab8bee53531f5bfa03330d4661ce3b
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-01-14 23:24:49 +0530

    priority queue

---
+++ BinaryHeap.java
@@ -0,0 +1,14 @@
+public class BinaryHeap {
+    public static void main(String[] args) {
+        /**
+         * 
+         * What is Prioroty Queue?
+         * Priority queue is a data structure that allows us to find minimum/maximum element among a collection
+         * of elements in constant time. It supports following operations -
+         *      1. insert(key) - Insert a key into the priority queue
+         *      2. deleteMax() / deleteMin() - return and remove largest / smallest key
+         *      3. getMax() / getMin() = return largest / smallest key
+         * 
+         */
+    }
+}
