c94c774c485a619dece372dc9bc2ac813f0a1042
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-01-25 22:00:12 +0530

    implemented binary search using iteration

--- SearchAlgorithms.java
+++ SearchAlgorithms.java
@@ -1,7 +1,6 @@
 public class SearchAlgorithms {
     public static void main(String[] args) {
-        // 1. Linear Search Algorithm
-        // 2. Binary Search Algorithm
+        // Linear Search Algorithm
         int[] arr = {5, 1, 9, 2, 10, 15, 20};
         int searchResult = linearSearch(arr, arr.length, 10);
         if (searchResult >= 0) {
@@ -9,6 +8,9 @@ public class SearchAlgorithms {
         } else {
             System.out.println("search element not found in the array");
         }
+
+        // Binary Search Algorithm - input array should be sorted
+        // It's a divide and conquer algorith which splits the array into two during each iteration reducing our searcn time n/2
     }
 
 
@@ -18,6 +20,30 @@ public class SearchAlgorithms {
                 return i;
             }
         }
+        return -1;
+    }
+
+    public static int binarySearch(int[] arr, int n, int x){
+        int low = 0; // start index
+        int high = n - 1; // last index
+
+        while (low <= high) {
+            int mid = (low + high)/2; // calculate mid index
+            if (arr[mid] == x) { // if search element is in the middle of the array
+                // return the mid index as output
+                return mid;
+            }
+
+            // if the element in middle index is less than the search element
+            if (arr[mid] < x) {
+                // update the lower index to mid+1, so that next search will happen only on second half
+                low = mid + 1; // updating lower index
+            } else {
+                // else update the high index to mid-1, so that next search will happen only in first half
+                high = mid - 1; // updating high index
+            }
+        }
+
         return -1;
     }
 }
