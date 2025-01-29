eba5226e7f63ee0ac421cf0041d50cfe325aa4b9
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-01-29 19:05:41 +0530

    sorting array

---
+++ SortingAlgorithms.java
@@ -0,0 +1,13 @@
+public class SortingAlgorithms {
+    public static void main(String[] args) {
+        // BUBBLE SORT Algorithm
+        //      1. It is also called as sinking sort
+        //      2. While applying this sorting algorithm on unsorted array, the largest elements tends to sink at the end of the array.
+        //      3. It repeatedly compares pair of adjacent elements and swap them if they are in wrong order
+        //      4. reference - https://youtu.be/2ZLl8GAk1X4?t=87325
+
+    }
+
+    
+
+}
