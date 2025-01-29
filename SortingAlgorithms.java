a25c2f62a93ac2267ffe669a0e62d5e60eba157f
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-01-29 20:34:06 +0530

    insertionSort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -10,6 +10,19 @@ public class SortingAlgorithms {
         sortingAlgorithms.bubbleSort(arr, arr.length);
         sortingAlgorithms.printArrayElements(arr);
 
+        // INSERTION SORT Algorithm
+        //      1. Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
+        //      2. In insertion sort, we divide the given array into two parts - sorted part and unsorted part.
+        //      3. From unsorted part, we take first element and place at its correct position in sorted array . This is
+        //         done by shifting all the elements which are larger than first element by one position.
+        //      4. Its repeated till unsorted array is not empty.
+        int[] arr2 = {7,6,5,4,3,2,1,0};
+        sortingAlgorithms.insertionSort(arr2, arr2.length);
+        sortingAlgorithms.printArrayElements(arr2);
+
+        // SELECTION SORT Algorithm
+        
+
     }
 
     public void bubbleSort(int[] arr, int n){
@@ -30,6 +43,23 @@ public class SortingAlgorithms {
             if (!isSwapped) {
                 break;
             }
+        }
+    }
+
+    public void insertionSort(int[] arr, int n){
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i]; // taking the first element
+            int j = i - 1; // index for the last element of sorted array
+
+            // traversal should continue till the first element in sorted array
+            // traveral should continue till the current element in sorted list is smaller than than first element
+            while (j >= 0 && arr[j] > temp) {
+                // if index >= 0 and the current element is greater than the first element
+                arr[j + 1] = arr[j]; // shift current element one index right
+                j--; // decrement j to point to previous element in the sorted array
+            }
+            // update current index with the first element
+            arr[j + 1] = temp;
         }
     }
 
