a2bef07372f4e8cff591d6da392ef0aeb678405e
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-02 22:51:28 +0530

    number of islands

--- Graphs.java
+++ Graphs.java
@@ -55,5 +55,54 @@ public class Graphs {
          graphs.addEdge(2, 3);
          graphs.addEdge(3, 0);
          System.out.println(graphs);
+
+         // Number of Islands - https://youtu.be/2ZLl8GAk1X4?t=122166
+         Graphs graphs2 = new Graphs(4);
+         graphs2.addEdge(0, 1);
+         graphs2.addEdge(0, 0);
+         graphs2.addEdge(2, 2);
+         graphs2.addEdge(3, 3);
+         System.out.println(graphs2);
+         System.out.println(graphs2.numIslands(graphs2.adjMatrix));
+
+    }
+
+
+    public int numIslands(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int numIslands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    dfs(grid, i, j, visited);
+                    numIslands++; // once we traverse through the islands, increment islands count
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+
+    public void dfs(int[][] grid, int row, int col, boolean[][] visited){
+        // base case
+        // 1. if row or col exceeds the actualy size of matrix (either negatively or positively)
+        // 2. if the current vertex is already visited.
+        // 3. if the current vertex is a water surrounding the island (0)
+        if (row < 0 || col < 0 ||
+            row >= grid.length || col >= grid[0].length ||
+            visited[row][col] || grid[row][col] == 0) {
+            return;
+        }
+
+        visited[row][col] = true; // since this node is visited
+
+        // recusive search on horizontal and vertical elements
+        dfs(grid, row, col-1, visited); // go left
+        dfs(grid, row-1, col, visited); // go up
+        dfs(grid, row, col+1, visited); // go right
+        dfs(grid, row+1, col, visited); // go down
     }
 }
