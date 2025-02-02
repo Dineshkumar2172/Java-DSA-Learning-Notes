0f6e0d097a499444776f4e9de13aeb1527a22aa4
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-02 23:22:58 +0530

    hash functions

--- Hashing.java
+++ Hashing.java
@@ -7,5 +7,12 @@ public class Hashing {
         //      3. The operation supported by hashing such as storing, retrieving and removing information have average
         //         runtime complexity of O(1).
 
+        // What are Hash Functions? - https://youtu.be/2ZLl8GAk1X4?t=125912
+        //      1. A hash function simply takes an arbitrary size key and provides fixed size value also called as index.
+        //      2. Commonly used hash function - modular hash function  - https://youtu.be/2ZLl8GAk1X4?t=126086
+        //              i. A modular hash function simply takes a key and size of array, returns remainder by dividing key by size.
+        //             ii. The remainder is used as an index to store the key in an array of provided size.
+
+
     }
 }
