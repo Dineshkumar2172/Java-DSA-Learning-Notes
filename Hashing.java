5f18f294da4be81bfca2838628ce354b4c539a4f
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-02 23:10:36 +0530

    what is hashing

---
+++ Hashing.java
@@ -0,0 +1,11 @@
+public class Hashing {
+    public static void main(String[] args) {
+        // What is Hashing? - https://youtu.be/2ZLl8GAk1X4?t=125041
+        //      1. Hashing is a technique used for storing, retrieving and removing information as quickly as possible.
+        //      2. It's a process of converting a arbitrary size key into fixed sized value. The conversion is
+        //         done via a special function called Hash function.
+        //      3. The operation supported by hashing such as storing, retrieving and removing information have average
+        //         runtime complexity of O(1).
+
+    }
+}
