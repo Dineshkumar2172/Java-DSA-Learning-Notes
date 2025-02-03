d87a3f7904fb7856827f7166af63ff7893f1e7d8
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-04 00:15:23 +0530

    insert and merge intervals

--- Interval.java
+++ Interval.java
@@ -1,4 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Interval {
+
+    public class IntervalStruct{
+        private int start;
+        private int end;
+        public IntervalStruct(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+
     public static void main(String[] args) {
         // Introduction to Intervals - https://youtu.be/2ZLl8GAk1X4?t=135946
         //      1. An interval is a range represented by two numbers like (5, 8)
@@ -17,5 +30,20 @@ public class Interval {
         //      4. a and b overlap, a ends after b.
         //      5. b completely overlaps a.
         //      6. b and a do not overlap.
+        //
+        //
+        // Merge Intervals - https://youtu.be/2ZLl8GAk1X4?t=136441
+        // Question 1: Given a list of intervals, merge all the overlapping intervals and
+        // return a list of non-overlapping intervals. Example:
+        // Input: intervals = [{2, 6}, {1, 3}, {8, 10}]
+        // Output: [{1, 6}, {8, 10}]
+        //
+        //
+        // Insert Intervals - https://youtu.be/2ZLl8GAk1X4?t=137763
+        // Question 2: Given a list of non-overlapping intervals sorted by their start time, insert
+        // a given interval at the valid position, merge all the overlapping intervals and return a list
+        // of mutually exclusive intervals. Example:
+        // Input: intervals - [{1, 3}, {5, 7}, {8, 10}], newInterval = [{4, 9}]
+        // Output: [{1, 3}, {4, 10}]
     }
-}
+} 
