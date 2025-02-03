c311cc344a5c5a3250df0b8b22a9285b688e1617
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-03 21:01:45 +0530

    intervals and overlapping intervals

---
+++ Interval.java
@@ -0,0 +1,21 @@
+public class Interval {
+    public static void main(String[] args) {
+        // Introduction to Intervals - https://youtu.be/2ZLl8GAk1X4?t=135946
+        //      1. An interval is a range represented by two numbers like (5, 8)
+        //      2. The two numbers are termed as - start and end.
+        //      3. Example - Time interval in tasks or jobs - 
+        //              a = (1, 3)
+        //              b = (4, 5)
+        //              c = (8, 10)
+        //              d = (9, 11)
+        //
+        // Overlapping Intervals - https://youtu.be/2ZLl8GAk1X4?t=136142
+        // Relationship between two intervals a and b
+        //      1. a and b do not overlap.
+        //      2. a and b overlap, b ends after a.
+        //      3. a completely overlaps b.
+        //      4. a and b overlap, a ends after b.
+        //      5. b completely overlaps a.
+        //      6. b and a do not overlap.
+    }
+}
