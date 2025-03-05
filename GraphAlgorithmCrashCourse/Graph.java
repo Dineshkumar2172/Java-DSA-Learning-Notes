ad7ca85658bf8d6eedb8a5149d64f4edf608e57a
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-05 22:14:52 +0530

    graph algorithms - objective

--- GraphAlgorithmCrashCourse/Graph.java
+++ GraphAlgorithmCrashCourse/Graph.java
@@ -1,5 +1,15 @@
 package GraphAlgorithmCrashCourse;
 
 public class Graph {
-    
+    public static void main(String[] args) {
+        // Circles are vertices that holds data and edges are links that connects them
+
+        // As part of this course, we'll be building a single class which should be able to support
+        // undirected, directed, connected and disconnected graphs.
+
+        // This course is gonna cover three main graph traversal algorithm
+        //      1. Depth First Search
+        //      2. Breadth First Search
+        //      3. Dijkstra's algorithm
+    }
 }
