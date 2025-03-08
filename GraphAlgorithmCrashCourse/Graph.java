edd5b0e8ce206faf6a05b2f038546dfe297af54b
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-08 17:30:45 +0530

    graph class implementation

--- GraphAlgorithmCrashCourse/Graph.java
+++ GraphAlgorithmCrashCourse/Graph.java
@@ -1,6 +1,75 @@
 package GraphAlgorithmCrashCourse;
 
+import java.util.ArrayList;
+
 public class Graph {
+
+    private ArrayList<Vertex> vertices;
+    private boolean isWeighted; // flag to say if its weighted graph
+    private boolean isDirected; // flag to say if its directed graph
+
+    public void graph(boolean inputIsWeighted, boolean inputIsDirected) {
+        this.vertices = new ArrayList<Vertex>();
+        this.isWeighted = inputIsWeighted;
+        this.isDirected = inputIsDirected;
+    }
+
+    public Vertex addVertex(String data) {
+        Vertex newVertex = new Vertex(data);
+        this.vertices.add(newVertex);
+        return newVertex;
+    }
+
+    public void addEdge(Vertex vertex1, Vertex vertex2, Integer weight){
+        if (!this.isWeighted) {
+            weight = null;
+        }
+
+        vertex1.addEdge(vertex2, weight);
+        if (!this.isDirected) { // incase of undirected, edges goes both ways
+            vertex2.addEdge(vertex1, weight);
+        }
+    }
+
+    public void removeEdge(Vertex vertex1, Vertex vertex2) {
+        vertex1.removeEdge(vertex2);
+        if(!this.isDirected) { // incase of undirected, remove edge in other half as well
+            vertex2.removeEdge(vertex1);
+        }
+    }
+
+    public void removeVertex(Vertex vertex) {
+        this.vertices.remove(vertex);
+    }
+
+    public ArrayList<Vertex> getVertices(){
+        return this.vertices;
+    }
+
+    public boolean isWeighted() {
+        return this.isWeighted;
+    }
+
+    public boolean isDirected() {
+        return this.isDirected;
+    }
+
+    public Vertex getVertexByValue(String value) {
+        for (Vertex vertex : vertices) {
+            if (vertex.getData() == value) {
+                return vertex;
+            }
+        }
+
+        return null;
+    }
+
+    public void print() {
+        for (Vertex vertex : vertices) {
+            vertex.print(isWeighted);
+        }
+    }
+
     public static void main(String[] args) {
         // Circles are vertices that holds data and edges are links that connects them
 
