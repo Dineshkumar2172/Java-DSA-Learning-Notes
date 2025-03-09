ca09e0f3a52641258fab2dbcc17bd6d987a174d4
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-09 19:05:10 +0530

    bipartite graph

---
+++ GraphPreparations/BipartiteGraph/BipartiteGraph.java
@@ -0,0 +1,7 @@
+package GraphPreparations.BipartiteGraph;
+
+public class BipartiteGraph {
+    // reference - https://youtu.be/-vu34sct1g8
+    // when a two adjacent vertices has a same color - then it's not bipartite
+    // when none of the adjacent vertices has a same color - then it's bipartite graph
+}
