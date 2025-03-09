ee3d5e1984a35358f5ea4291544c458cd6b88ca0
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-09 14:06:42 +0530

    AdjacencyList representation implementation

--- GraphPreparations/GraphRepresentations/AdjacencyList.java
+++ GraphPreparations/GraphRepresentations/AdjacencyList.java
@@ -1,5 +1,84 @@
 package GraphPreparations.GraphRepresentations;
 
+import java.util.LinkedList;
+
 public class AdjacencyList {
-    private 
+    // representation
+    //      1. its an array where each index represents each vertex
+    //      2. Each of these indices i contains a linkedlist where each node represents the
+    //         vertices that are connected to the vertex i.
+    //      3. Operations include addEdge, removeEdge, displayGraph, some validations.
+
+
+    // Improvement: Could use List<Set<Integer>> to handle duplicate edges
+    private LinkedList<Integer>[] adjList;
+    private boolean isUndirectedGraph;
+
+    public AdjacencyList(int v, boolean isUndirectedGraph) {
+        this.adjList = new LinkedList[v]; // initialising array with v (number of vertices)
+        this.isUndirectedGraph = isUndirectedGraph;
+
+        for (int i = 0; i < v; i++) {
+            this.adjList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int startVertex, int endVertex) {
+        // method responsible for adding edge between vertices
+        if (!isValidVertex(startVertex) || !isValidVertex(endVertex)) {
+            System.out.println("Invalid vertex: Reverify the entered vertex");
+            return;
+        }
+
+        if (startVertex == endVertex) {
+            System.out.println("Invalid action");
+            return;
+        }
+
+        this.adjList[startVertex].add(endVertex);
+        if (isUndirectedGraph) {
+            this.adjList[endVertex].add(startVertex);
+        }
+    }
+
+    public void removeEdge(int startVertex, int endVertex) {
+        // method responsible for removing edges between vertices
+        if (!isValidVertex(startVertex) || !isValidVertex(endVertex)) {
+            System.out.println("Invalid vertex: Reverify the entered vertex");
+            return;
+        }
+
+        if (startVertex == endVertex) {
+            System.out.println("Invalid action");
+            return;
+        }
+
+        this.adjList[startVertex].remove(endVertex);
+        if (isUndirectedGraph) {
+            this.adjList[endVertex].remove(startVertex);
+        }
+    }
+
+    public void displayGraph() {
+        for (int i = 0; i < adjList.length; i++) {
+            System.out.print("Vertex " + i + " --> ");
+            // iterating over all the elements from an LinkedList of integer elements
+            for (Integer vertex : adjList[i]) {
+                System.out.print(vertex + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isValidVertex(int v) {
+        return v >= 0 && v < adjList.length;
+    }
+
+    public static void main(String[] args) {
+        AdjacencyList adjacencyList = new AdjacencyList(4, true);
+        adjacencyList.addEdge(0, 1);
+        adjacencyList.addEdge(3, 2);
+        adjacencyList.addEdge(0, 2);
+        adjacencyList.displayGraph();
+    }
 }
