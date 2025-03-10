617eae418f54c6154675f164c9af92fc359cdcda
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-11 00:06:50 +0530

    bellman-ford algorithm in progress

--- GraphPreparations/week2/BellmanFordAlgorithm/BellmanFordAlgorithm.java
+++ GraphPreparations/week2/BellmanFordAlgorithm/BellmanFordAlgorithm.java
@@ -1,5 +1,23 @@
 package GraphPreparations.week2.BellmanFordAlgorithm;
 
+import GraphPreparations.week2.WeightedAdjacencyList;
+
 public class BellmanFordAlgorithm {
-    
+
+    public void executeBellmanFord() {
+
+    }
+
+    public static void main(String[] args) {
+        WeightedAdjacencyList weightedAdjacencyList = new WeightedAdjacencyList(6, true);
+        weightedAdjacencyList.addEdge(0, 1, 4);
+        weightedAdjacencyList.addEdge(0, 2, 4);
+        weightedAdjacencyList.addEdge(1, 2, 2);
+        weightedAdjacencyList.addEdge(2, 3, 3);
+        weightedAdjacencyList.addEdge(2, 4, 1);
+        weightedAdjacencyList.addEdge(3, 5, 2);
+        weightedAdjacencyList.addEdge(4, 5, 3);
+        
+        BellmanFordAlgorithm bellmanFordAlgorithm = new BellmanFordAlgorithm();
+    }
 }
