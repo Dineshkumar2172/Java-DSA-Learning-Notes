315e1a1d1bce9531779c661c1d51fb8179aad61b
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-11 00:25:01 +0530

    bellman-ford algorithm in progress

--- GraphPreparations/week2/BellmanFordAlgorithm/BellmanFordAlgorithm.java
+++ GraphPreparations/week2/BellmanFordAlgorithm/BellmanFordAlgorithm.java
@@ -4,8 +4,13 @@ import GraphPreparations.week2.WeightedAdjacencyList;
 
 public class BellmanFordAlgorithm {
 
+    // Step 1: Initialize distances: {0, ∞, ∞, ∞, ∞}
+    // Step 2: Relax all edges V-1 times.
+    // Step 3: Check if any edge can be further relaxed → If yes, there is a negative cycle.
+    // Step 4: Print the shortest distances.
+
     public void executeBellmanFord() {
-        
+
     }
 
     public static void main(String[] args) {
