99e2b74a578a0aadcd59262029c131733c1ba1bf
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-15 13:30:02 +0530

    floyd warshall - implemented - day 4

--- GraphPreparations/practice/day4/FloydWarshall.java
+++ GraphPreparations/practice/day4/FloydWarshall.java
@@ -1,5 +1,61 @@
 package GraphPreparations.practice.day4;
 
 public class FloydWarshall {
-    
+
+    static final Integer INF = Integer.MAX_VALUE;
+
+    // all pair shortest path - works by relaxation
+    // finds shortest path from all vertices to all other vertices
+    public void executeFloydWarshall(int[][] graph) {
+        int vertices = graph.length;
+        int[][] shortestPath = new int[vertices][vertices];
+
+        // build initial shortest path matrix
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                shortestPath[i][j] = graph[i][j];
+            }
+        }
+
+        for (int k = 0; k < vertices; k++) { // intermediate vertex
+            for (int i = 0; i < vertices; i++) { // source vertex
+                for (int j = 0; j < vertices; j++) { // destination vertex
+                    if (shortestPath[i][k] != INF && shortestPath[k][j] != INF) {
+                            shortestPath[i][j] = Math.min(shortestPath[i][j], shortestPath[i][k] + shortestPath[k][j]);        
+                    }
+                }
+            }
+        }
+
+        // Detect negative cycles (if any diagonal element becomes negative)
+        for (int i = 0; i < vertices; i++) {
+            if (shortestPath[i][i] < 0) {
+                System.out.println("Negative cycle detected!");
+                return;
+            }
+        }
+
+        System.out.println("Shortest distances between every pair of vertices:");
+        for (int i = 0; i < shortestPath.length; i++) {
+            for (int j = 0; j < shortestPath[i].length; j++) {
+                if (shortestPath[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(shortestPath[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+            {0, 3, INF, 5},
+            {2, 0, INF, 4},
+            {INF, 1, 0, INF},
+            {INF, INF, 2, 0}
+        };
+
+        FloydWarshall floydWarshall = new FloydWarshall();
+        floydWarshall.executeFloydWarshall(graph);
+    }
 }
--- GraphPreparations/practice/day4/TopologicalSort.java
+++ GraphPreparations/practice/day4/TopologicalSort.java
@@ -1,5 +1,85 @@
 package GraphPreparations.practice.day4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+import GraphPreparations.practice.day1.AdjacencyList;
+
 public class TopologicalSort {
-    
+
+    public void executeTopoSortDFS(int vertex, boolean[] visitedVertices, AdjacencyList adjacencyList, Stack<Integer> stack) {
+        if (visitedVertices[vertex]) return;
+
+        visitedVertices[vertex] = true;
+        for (Integer[] neighbor: adjacencyList.getAdjacencyList().get(vertex)) {
+            if (!visitedVertices[neighbor[0]]) {
+                executeTopoSortDFS(neighbor[0], visitedVertices, adjacencyList, stack);
+            }
+        }
+
+        stack.push(vertex);
+    }
+
+    public void executeTopologicalSort(AdjacencyList adjacencyList) {
+        boolean[] visitedVertices = new boolean[adjacencyList.getNumberOfVertices()];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < adjacencyList.getNumberOfVertices(); i++) {
+            if (!visitedVertices[i]) {
+                executeTopoSortDFS(i, visitedVertices, adjacencyList, stack);
+            }
+        }
+
+        System.out.println("Topological sort in DFS : ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + ", ");
+        }
+        System.out.println();
+    }
+
+    public void executeTopologicalSortBFS(AdjacencyList adjacencyList) {
+        // uses indegree operation
+        int[] inDegree = new int[adjacencyList.getNumberOfVertices()];
+        for (int i = 0; i < adjacencyList.getNumberOfVertices(); i++) {
+            for (Integer[] neigh: adjacencyList.getAdjacencyList().get(i)) {
+                inDegree[neigh[0]]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < adjacencyList.getNumberOfVertices(); i++) {
+            // update vertices with indegree 0 to queue to cover independent tasks
+            if (inDegree[i] == 0) queue.offer(i);
+        }
+
+        List<Integer> topoOrder = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            topoOrder.add(current);
+            for (Integer[] neigh: adjacencyList.getAdjacencyList().get(current)) {
+                if (--inDegree[neigh[0]] == 0) {
+                    queue.add(neigh[0]);
+                }
+            }
+        }
+
+        System.out.println("Topological order of given graph : " + topoOrder);
+    }
+
+    public static void main(String[] args) {
+        AdjacencyList adjacencyList = new AdjacencyList(6, false, false);
+        adjacencyList.addEdge(5, 2);
+        adjacencyList.addEdge(5, 0);
+        adjacencyList.addEdge(4, 0);
+        adjacencyList.addEdge(4, 1);
+        adjacencyList.addEdge(2, 3);
+        adjacencyList.addEdge(3, 1);
+
+        TopologicalSort topologicalSort = new TopologicalSort();
+        topologicalSort.executeTopologicalSort(adjacencyList);
+        topologicalSort.executeTopologicalSortBFS(adjacencyList);
+    }
 }
