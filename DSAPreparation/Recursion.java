c68dd4123928b7ce0185864c270cc9882b3bdf7a
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-24 20:44:12 +0530

    sum upto N

--- DSAPreparation/Recursion.java
+++ DSAPreparation/Recursion.java
@@ -3,6 +3,7 @@ package DSAPreparation;
 public class Recursion {
 
     int counter = 0;
+    static int sumN = 0;
 
     public void print() {
         System.out.println("printing for : " + counter);
@@ -13,8 +14,20 @@ public class Recursion {
         }
     }
 
+    public void sumUptoN(int n) {
+        // write a recursive function that given an input n sums all nonnegative integers up to n.
+        // how would you solve it recusively?
+
+        if (n > 0) {
+            sumN += n;
+            sumUptoN(n-1);
+        }
+    }
+
     public static void main(String[] args) {
         Recursion recursion = new Recursion();
         recursion.print();
+        recursion.sumUptoN(5);
+        System.out.println(sumN);
     }
 }
