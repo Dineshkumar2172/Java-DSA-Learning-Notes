3f5a22eb1700b684efc81cefc8ffd132980e882c
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-03 17:49:15 +0530

    functional recursion

--- DynamicProgramming/StriverRe3.java
+++ DynamicProgramming/StriverRe3.java
@@ -19,7 +19,23 @@ public class StriverRe3 {
         firstNNumbers(n-1, summ);
     }
 
+    // 2. Summation of first n numbers - using functional recursion
+    //
+    // first stack  - 5 + f(4)  --> return 5 + 10 = 15
+    // second stack - 4 + f(3)  --> return 4 + 6 = 10
+    // third stack  - 3 + f(2)  --> return 3 + 3 = 6
+    // fourth stack - 2 + f(1)  --> return 2 + 1 = 3
+    // fifth stack  - 1 + f(0)  --> return 1 + 0 = 1
+    //
+    // f(0) - base case hit ==> return
+    public static int firstNNumbers(int n) {
+        if (n < 1) return 0;
+
+        return n + firstNNumbers(n - 1);
+    }
+
     public static void main(String[] args) {
-        firstNNumbers(5, 0);
+        firstNNumbers(5, 0); // parameterised recursion - we passed state from one stack to another as parameter
+        System.out.println(firstNNumbers(5)); // functional recursion - where instead of passing some state as parameters, we make function to return something.
     }
 }
