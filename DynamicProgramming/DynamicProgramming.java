182d580be5d459c22ecbd11beaf1f47db2937a3b
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-04 00:50:58 +0530

    fibonacci

--- DynamicProgramming/DynamicProgramming.java
+++ DynamicProgramming/DynamicProgramming.java
@@ -6,6 +6,10 @@ public class DynamicProgramming {
     //      Part 1: Memorization
     //      Part 2: Tabulation
 
+
+    // overlapping sub problems - whenever we end up solving same problems again and again.
+    // For example: in the below finbonacci function, assume we are calculating fibonacci of 8, here we perform f(2) multiple times within it.
+
     public static int getFibonacciN(int n) {
         if (n <= 1) return 1;
         return getFibonacciN(n-1) + getFibonacciN(n-2);
