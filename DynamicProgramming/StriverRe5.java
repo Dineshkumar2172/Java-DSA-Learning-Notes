1e40e6d5f0d71c13e100828980a8ba163d893df8
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-03 20:07:41 +0530

    multiple recursive call

--- DynamicProgramming/StriverRe5.java
+++ DynamicProgramming/StriverRe5.java
@@ -5,12 +5,24 @@ public class StriverRe5 {
 
     // 1. Write a function to calculate nth number in fibonacci series.
     public static int calculateFibonacci(int n) {
+        // Time complexity - (2^n) exponential
         if (n <= 1) return n;
 
         int left = calculateFibonacci(n-1);
         int right = calculateFibonacci(n-2);
         return left + right;
     }
+
+    // we can write the above code like this as well.
+    // calculateFibonacci(n-1) will get executed at first
+    // calculateFibonacci(n-2) will get executed after
+    // these recursive function won't get executed simultaneously
+    // public static int calculateFibonacci(int n) {
+    //     // Time complexity - (2^n) exponential
+    //     if (n <= 1) return n;
+
+    //     return calculateFibonacci(n-1) + calculateFibonacci(n-2);
+    // }
 
     public static void main(String[] args) {
         System.out.println(calculateFibonacci(6));
