e546616368d3ce85cc79dbc757b7281f972b223c
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-03 19:38:39 +0530

    is palindrome using recursion

--- DynamicProgramming/StriverRe4.java
+++ DynamicProgramming/StriverRe4.java
@@ -15,9 +15,26 @@ public class StriverRe4 {
     // tradeoff is we need to calculate the corresponding pointer ourselves using n - i - 1 with base condition i < n/2.
 
 
+    // 2. String is a palindrome of not.
+    // return true - if given input is a palindrome
+    // return false - if given input is not a palindrome
+    public static boolean isPalindrome(int left, int right, String word) {
+        if (left >= right) return true;
+        if (word.charAt(left) != word.charAt(right)) return false;
+
+        return isPalindrome(left+1, right-1, word);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
         reverseArray(arr, 0, arr.length - 1);
         for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + ", ");
+        System.out.println();
+
+        // String word = "mom";
+        String word = "ranoonar";
+        // String word = "mnsenm";
+        System.out.println(isPalindrome(0, word.length() - 1, word));
+
     }
 }
