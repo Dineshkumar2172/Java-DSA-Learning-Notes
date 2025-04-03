7122fadb40eb4a60fc7dca4d5dbbb32a1e55d0d2
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-04 00:53:38 +0530

    fibonacci

--- DynamicProgramming/DynamicProgramming.java
+++ DynamicProgramming/DynamicProgramming.java
@@ -8,7 +8,8 @@ public class DynamicProgramming {
 
 
     // overlapping sub problems - whenever we end up solving same problems again and again.
-    // For example: in the below finbonacci function, assume we are calculating fibonacci of 8, here we perform f(2) multiple times within it.
+    // For example: in the below finbonacci function, assume we are calculating fibonacci of 8, here we perform f(2) and others multiple times within it.
+    // why to calculate again if we already have calculated it? This is where memoization comes in - we tend to store the values of sub problems in some map/table.
 
     public static int getFibonacciN(int n) {
         if (n <= 1) return 1;
