45481b51322075e018e37827b01b9ac71ce6e5d3
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-04 00:02:52 +0530

    Printing single subsequences whose sum is k

--- DynamicProgramming/Recursion/StriverL7.java
+++ DynamicProgramming/Recursion/StriverL7.java
@@ -23,8 +23,34 @@ public class StriverL7 {
         subseqEqualK(idx+1, nums, curr, summ, k);
     }
 
+    // 2. Printing single subsequences whose sum is k
+    public static boolean singleSubseqEqualK(int idx, int[] nums, List<Integer> curr, int summ, int k) {
+        if (idx >= nums.length) {
+            if (summ == k) {
+                System.out.println(curr.toString());
+                return true;
+            }
+
+            return false;
+        }
+
+        // pick
+        curr.add(nums[idx]);
+        summ += nums[idx];
+        if(singleSubseqEqualK(idx+1, nums, curr, summ, k)) return true;
+
+        // not pick
+        curr.remove(curr.size() - 1);
+        summ -= nums[idx];
+        if(singleSubseqEqualK(idx+1, nums, curr, summ, k)) return true;
+        
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] input = {1, 2, 1};
         subseqEqualK(0, input, new ArrayList<>(), 0, 2);
+        System.out.println("Finding only one answer : ");
+        singleSubseqEqualK(0, input, new ArrayList<>(), 0, 2);
     }
 }
