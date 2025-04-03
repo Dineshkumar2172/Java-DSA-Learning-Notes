1bd8082dfe831ab2afbf9788419bd324f33216c8
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-03 17:16:33 +0530

    backtracking

--- DynamicProgramming/StriverRe2.java
+++ DynamicProgramming/StriverRe2.java
@@ -26,16 +26,28 @@ public class StriverRe2 {
     public static void printNReverse(int i) {
         // Time complexity - 0(n) - we are calling same function n times
         // Space complexity - 0(n)- there will be n new stacks created.
-        if (i <= 0) return;
+        if (i <= 1) return;
 
         System.out.println(i);
         printNReverse(i - 1);
+    }
+
+    // 3. Print from 1 to n, but using +1 is not allowed (use backtracking)
+    public static void print1Restricted(int n) {
+        if (n < 1) return;
+        
+        
+        print1Restricted(n - 1);
+        System.out.println(n);
     }
 
     public static void main(String[] args) {
         printName("Dinesh", 0, 5);
         printLinearN(1, 5);
         printNReverse(5);
+
+        // backtracking
+        print1Restricted(5);
     }
 }
 
