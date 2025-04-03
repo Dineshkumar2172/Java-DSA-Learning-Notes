890d820172f806cf525deff17bbec98ed599fdac
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-04 00:30:06 +0530

    fibonacci

--- DynamicProgramming/DynamicProgramming.java
+++ DynamicProgramming/DynamicProgramming.java
@@ -6,7 +6,12 @@ public class DynamicProgramming {
     //      Part 1: Memorization
     //      Part 2: Tabulation
 
+    public static int getFibonacciN(int n) {
+        if (n <= 1) return 1;
+        return getFibonacciN(n-1) + getFibonacciN(n-2);
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(getFibonacciN(5));
     }
 }
