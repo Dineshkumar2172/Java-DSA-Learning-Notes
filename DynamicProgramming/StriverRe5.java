174eda947a9989433526d5421941ed3cd8cd80b5
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-03 20:03:46 +0530

    multiple recursive call

--- DynamicProgramming/StriverRe5.java
+++ DynamicProgramming/StriverRe5.java
@@ -1,7 +1,18 @@
 package DynamicProgramming;
 
 public class StriverRe5 {
+    // reference: https://youtu.be/kvRjNm4rVBE?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
+
+    // 1. Write a function to calculate nth number in fibonacci series.
+    public static int calculateFibonacci(int n) {
+        if (n <= 1) return n;
+
+        int left = calculateFibonacci(n-1);
+        int right = calculateFibonacci(n-2);
+        return left + right;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(calculateFibonacci(6));
     }
 }
