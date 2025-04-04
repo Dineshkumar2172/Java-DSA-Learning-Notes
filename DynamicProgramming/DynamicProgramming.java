9318287fdb2f268f2d700266d6b32e1ceae0f8d7
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-04 13:38:01 +0530

    tabulation implementation

--- DynamicProgramming/DynamicProgramming.java
+++ DynamicProgramming/DynamicProgramming.java
@@ -40,11 +40,37 @@ public class DynamicProgramming {
     //     return left + right;
     // }
 
+
     public static void main(String[] args) {
         int n = 6;
         System.out.println(getFibonacciN(n));
 
         int[] dp = new int[n + 1];
         System.out.println(getFibonacciDynamic(n, dp));
+
+
+        // converted fibonacci from DP memoization to tabulation - bottom up approach
+        // Space complexity - O(n) - not stack space - efficient
+        // Time complexity - O(n) - perform n times.
+        dp = new int[n + 1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        for (int ele: dp) {
+            System.out.println(ele);
+        }
+
+        // much efficient with space
+        // Space complexity - O(1)
+        // Time complexity - O(n)
+        int previousFirst = 0;
+        int previousSecond = 1;
+        for (int i = 1; i < n; i++) {
+            int currentElement = previousFirst + previousSecond;
+            System.out.println(currentElement);
+            previousFirst = previousSecond;
+            previousSecond = currentElement;
+        }
     }
 }
