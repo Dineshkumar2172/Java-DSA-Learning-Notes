f40631ee2ee042e25982fc2b7d1b34aa6edcb8d1
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-06 23:26:15 +0530

    intervals refresh

--- Intervals/Intervals.java
+++ Intervals/Intervals.java
@@ -1,4 +1,22 @@
 public class Intervals {
+
+    /**
+     * Interval is a range represented by two numbers start and end - [start, end] (can be any unit of time)
+     * 
+     * 
+     * OVERLAPPING INTERVALS
+     * 
+     * 1. Assume we have two intervals (1, 5) and (2, 6) - (1, 5) is overlapping with (2, 6)
+     * 
+     * 
+     *   i) a and b do not overlap.
+     *  ii) a and b overlap, b ends after a
+     * iii) a completely overlaps b.
+     *  iv) a and b overlaps, a ends after b
+     *   v) b completely overlaps a
+     *  vi) b do not overlap a
+     */
+
     public static void main(String[] args) {
         
     }
