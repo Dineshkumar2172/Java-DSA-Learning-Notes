8115b5de6b5b8abd9e74b878d30495ff0d09d049
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-13 19:05:30 +0530

    topological sort

--- GraphPreparations/practice/day11/TopologicalSorting.java
+++ GraphPreparations/practice/day11/TopologicalSorting.java
@@ -9,15 +9,59 @@ import java.util.Stack;
 public class TopologicalSorting {
 
     public void dfs(Integer vertex, boolean[] visitedVertices, Stack<Integer> stack, AdjacencyList adjacencyList) {
-        
+        if (visitedVertices[vertex]) return;
+
+        visitedVertices[vertex] = true;
+        for (Integer[] neighbor: adjacencyList.getAdjacencyList().get(vertex)) {
+            if (!visitedVertices[neighbor[0]]) {
+                dfs(neighbor[0], visitedVertices, stack, adjacencyList);
+            }
+        }
+
+        stack.push(vertex);
     }
 
     public void executeTopologicalSortDFS(AdjacencyList adjacencyList) {
-       
+        boolean[] visited = new boolean[adjacencyList.getNumberOfVertices()];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < adjacencyList.getNumberOfVertices(); i++) {
+            if (!visited[i]) {
+                dfs(i, visited, stack, adjacencyList);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + ", ");
+        }
     }
 
     public void executeTopologicalSortBFS(AdjacencyList adjacencyList) {
-        
+        System.out.println();
+        int n = adjacencyList.getNumberOfVertices();
+        int[] inDegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (Integer[] edge: adjacencyList.getAdjacencyList().get(i)) {
+                inDegree[edge[0]]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + ", ");
+            for (Integer[] edge: adjacencyList.getAdjacencyList().get(current)) {
+                if (--inDegree[edge[0]] == 0) {
+                    queue.add(edge[0]);
+                }
+            }
+        }
     }
 
 
