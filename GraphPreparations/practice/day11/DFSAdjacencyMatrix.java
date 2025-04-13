e6e1ef8274a8a73dc7a289673ce0d1ceda8a3b8a
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-13 17:26:39 +0530

    DFS matrix

--- GraphPreparations/practice/day11/DFSAdjacencyMatrix.java
+++ GraphPreparations/practice/day11/DFSAdjacencyMatrix.java
@@ -5,11 +5,37 @@ import java.util.Stack;
 public class DFSAdjacencyMatrix {
     
     public void executeDFSIterative(Integer startVertex, AdjacencyMatrix adjacencyMatrix) {
-       
+        int vertex = adjacencyMatrix.getVertexCount();
+        boolean[] visited = new boolean[vertex];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(startVertex);
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            if (visited[current]) continue;
+
+            visited[current] = true;
+            System.out.print(current + ", ");
+            for (int i = 0; i < vertex; i++) {
+                if (adjacencyMatrix.getAdjacencyMatrix()[current][i] > 0 && !visited[i]) {
+                    stack.push(i);
+                }
+            }
+        }
+
+        System.out.println();
     }
 
     public void executeDFSRecursive(Integer vertex, AdjacencyMatrix adjacencyMatrix, boolean[] visitedvertices) {
-        
+        if(visitedvertices[vertex]) return;
+
+        visitedvertices[vertex] = true;
+        System.out.print(vertex + ", ");
+        for (int i = 0; i < adjacencyMatrix.getVertexCount(); i++) {
+            if (adjacencyMatrix.getAdjacencyMatrix()[vertex][i] != 0 && !visitedvertices[i]) {
+                executeDFSRecursive(i, adjacencyMatrix, visitedvertices);
+            }
+        }
     }
 
     public static void main(String[] args) {
