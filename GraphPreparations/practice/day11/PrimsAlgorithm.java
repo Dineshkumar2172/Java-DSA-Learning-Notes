a7a52d1a7b08c2e99cf8dec20c0eaa0f748eb491
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-13 18:30:46 +0530

    kruskal

--- GraphPreparations/practice/day11/PrimsAlgorithm.java
+++ GraphPreparations/practice/day11/PrimsAlgorithm.java
@@ -11,7 +11,43 @@ public class PrimsAlgorithm {
     // 3. We use parent array, visited vertices array and minEdgeWeight arrray.
     // 4. It works on undirected and weighted graphs.
     public void executePrims(int startVertex, AdjacencyList adjacencyList) {
-       
+        PriorityQueue<Integer[]> pq = new PriorityQueue<>(Comparator.comparing(edge -> edge[1])); // mind heap based on distance
+        Integer[] minMstWeights = new Integer[adjacencyList.getNumberOfVertices()];
+        boolean[] visited = new boolean[adjacencyList.getNumberOfVertices()];
+        Integer[] parent = new Integer[adjacencyList.getNumberOfVertices()];
+
+        Arrays.fill(minMstWeights, Integer.MAX_VALUE);
+        Arrays.fill(parent, -1);
+
+        minMstWeights[startVertex] = 0;
+        pq.add(new Integer[]{startVertex, 0}); // vertex, distance
+
+        int mstWeight = 0;
+        while (!pq.isEmpty()) {
+            Integer[] current = pq.poll();
+
+            if (visited[current[0]]) continue;
+
+            visited[current[0]] = true;
+            mstWeight += current[1];
+
+            for (Integer[] neigh: adjacencyList.getAdjacencyList().get(current[0])) {
+                int neighVertex = neigh[0], neighWeight = neigh[1];
+                if (!visited[neighVertex] && neighWeight < minMstWeights[neighVertex]) {
+                    minMstWeights[neighVertex] = neighWeight;
+                    pq.add(new Integer[]{neighVertex, minMstWeights[neighVertex]});
+                    parent[neighVertex] = current[0];
+                }
+            }
+        }
+
+        System.out.println("Minimum spanning tree edges : ");
+        for (int i = 0; i < adjacencyList.getNumberOfVertices(); i++) {
+            if (parent[i] != -1) {
+                System.out.println(parent[i] + " - " + " (weight: " + minMstWeights[i] + ")");
+            }
+        }
+        System.out.println("Total weight of MST: " + mstWeight);
     }
 
     public void executePrimsMatrix(Integer startVertex, AdjacencyMatrix adjacencyMatrix) {
