97d5c993883274d7b0d9665f2fe3f621e638c3c4
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-13 17:47:22 +0530

    floyd warshall

--- GraphPreparations/practice/day11/FloydWarshall.java
+++ GraphPreparations/practice/day11/FloydWarshall.java
@@ -8,7 +8,49 @@ public class FloydWarshall {
     public static final Integer INF = Integer.MAX_VALUE;
 
     public void executeFloydWarshall(AdjacencyMatrix adjacencyMatrix) {
+        final int N = adjacencyMatrix.getVertexCount();
+        int[][] shortestPath = new int[N][N];
         
+        // creating matrix representation for floyd warshall
+        // 1. diagonals should be 0
+        // 2. No edges should be represented as INF
+        // 3. Edges should be represented with weights
+        for (int i = 0; i < adjacencyMatrix.getVertexCount(); i++) {
+            for (int j = 0; j < adjacencyMatrix.getVertexCount(); j++) {
+                if (i == j) {
+                    shortestPath[i][j] = 0; // Distance to itself is 0
+                } else if (adjacencyMatrix.getAdjacencyMatrix()[i][j] != 0) {
+                    shortestPath[i][j] = adjacencyMatrix.getAdjacencyMatrix()[i][j];
+                } else {
+                    shortestPath[i][j] = INF;
+                }
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (shortestPath[i][k] != INF && shortestPath[k][j] != INF) {
+                        shortestPath[i][j] = Math.min(shortestPath[i][j], shortestPath[i][k] + shortestPath[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (shortestPath[i][i] < 0) {
+                System.out.println("negative cycle detected");
+                return;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print((shortestPath[i][j] != INF ? shortestPath[i][j] : "INF") + ", ");
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
