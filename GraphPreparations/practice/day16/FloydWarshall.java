6e247adff95ad9a695378ffdf2f4554bd5cb7492
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-05-04 22:40:08 +0530

    floyd warshall implementation

--- GraphPreparations/practice/day16/FloydWarshall.java
+++ GraphPreparations/practice/day16/FloydWarshall.java
@@ -1,5 +1,7 @@
 package GraphPreparations.practice.day16;
 
+import java.util.Arrays;
+
 public class FloydWarshall {
     // 1. It's a all pair shortest path algorithm - find shortest path from all vertices to all others.
     // 2. It's based on dynamic programming and based on matrix representation.
@@ -8,7 +10,47 @@ public class FloydWarshall {
     public static final Integer INF = Integer.MAX_VALUE;
 
     public void executeFloydWarshall(AdjacencyMatrix adjacencyMatrix) {
+        int n = adjacencyMatrix.getVertexCount();
+        Integer[][] shortestDistance = new Integer[n][n];
         
+        for (int i = 0; i < adjacencyMatrix.getVertexCount(); i++) {
+            for (int j = 0; j < adjacencyMatrix.getVertexCount(); j++) {
+                if (i == j) {
+                    shortestDistance[i][j] = 0; // Distance to itself is 0
+                } else if (adjacencyMatrix.getAdjacencyMatrix()[i][j] != 0) {
+                    shortestDistance[i][j] = adjacencyMatrix.getAdjacencyMatrix()[i][j];
+                } else {
+                    shortestDistance[i][j] = INF;
+                }
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (shortestDistance[i][k] != INF && shortestDistance[k][j] != INF){
+                        shortestDistance[i][j] = Math.min(
+                                shortestDistance[i][j], 
+                                shortestDistance[i][k] + shortestDistance[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (shortestDistance[i][i] < 0) {
+                System.out.println("negative cycle detected");
+                return;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print((shortestDistance[i][j] != INF ? shortestDistance[i][j] : "INF") + ", ");
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
